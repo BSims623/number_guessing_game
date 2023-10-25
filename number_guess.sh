7b303e315c5a1576f966065f2019000c7a53a751
Brian Sims <briansims623@gmail.com>
2023-10-25 01:59:22 -0400

    feat: MAIN_MENU SECRET_NUMBER

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo -e "\n~~~~ Welcome to The Number Guessing Game! ~~~~\n"
+
+SECRET_NUMBER=$((RANDOM % 1000 + 1))
+
+COUNT=0
+
+MAIN_MENU() {
+  if [[ $1 ]]
+  then 
+    echo -e "\n$1\n"
+  fi  
+  echo Enter your username:
+  read USERNAME
+  if [[ ! $USERNAME =~ ^.{1,22}$ ]]
+  then 
+    MAIN_MENU "Enter a username that is 22 characters or less"
+  else 
+    GET_USERNAME_INFO "$USERNAME"  
+  fi  
+}
+
+GET_USERNAME_INFO() {
+    echo "$1"
+}
+
+MAIN_MENU
